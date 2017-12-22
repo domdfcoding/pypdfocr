8903118d9586c6af882537efee093494efddc3ca
Dominic Davis-Foster <domdfcoding@gmail.com>
2017-12-22 22:53:43 +0000

    Updated to remove filing options

--- pypdfocr/pypdfocr.py
+++ pypdfocr/pypdfocr.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import smtplib
 import argparse
 import sys, os, traceback, time
 import logging
@@ -35,9 +34,6 @@ from pypdfocr_pdf import PyPdf
 from pypdfocr_tesseract import PyTesseract
 from pypdfocr_gs import PyGs
 from pypdfocr_watcher import PyPdfWatcher
-from pypdfocr_pdffiler import PyPdfFiler
-from pypdfocr_filer_dirs import PyFilerDirs
-from pypdfocr_filer_evernote import PyFilerEvernote
 from pypdfocr_preprocess import PyPreprocess
 
 def error(text):
@@ -79,8 +75,6 @@ class PyPDFOCR(object):
             * Runs ghostscript to get tiff/jpg
             * Runs Tesseract-OCR to do the actual OCR
             * Takes the HOCR from Tesseract and creates a new PDF with the text overlay
-        * Files the OCR'ed file in the proper place if specified
-        * Files the original file if specified
         * 
     """
 
@@ -132,9 +126,6 @@ class PyPDFOCR(object):
         p.add_argument('-v', '--verbose', action='store_true',
             default=False, dest='verbose', help='Turn on verbose mode')
 
-        p.add_argument('-m', '--mail', action='store_true',
-            default=False, dest='mail', help='Send email after conversion')
-
         p.add_argument('-l', '--lang',
             default='eng', dest='lang', help='Language(default eng)')
 
@@ -155,22 +146,7 @@ class PyPDFOCR(object):
         single_or_watch_group.add_argument('-w', '--watch', 
              dest='watch_dir', help='Watch given directory and run ocr automatically until terminated')
 
-        #-----------
-        # Filing options
-        #----------
-        filing_group = p.add_argument_group(title="Filing optinos")
-        filing_group.add_argument('-f', '--file', action='store_true',
-            default=False, dest='enable_filing', help='Enable filing of converted PDFs')
-        #filing_group.add_argument('-c', '--config', type = argparse.FileType('r'),
-        filing_group.add_argument('-c', '--config', type = lambda x: open_file_with_timeout(p,x),
-             dest='configfile', help='Configuration file for defaults and PDF filing')
-        filing_group.add_argument('-e', '--evernote', action='store_true',
-            default=False, dest='enable_evernote', help='Enable filing to Evernote')
-        filing_group.add_argument('-n', action='store_true',
-            default=False, dest='match_using_filename', help='Use filename to match if contents did not match anything, before filing to default folder')
-
-
-        # Add flow option to single mode extract_images,preprocess,ocr,write
+         # Add flow option to single mode extract_images,preprocess,ocr,write
 
         args = p.parse_args(argv)
 
@@ -179,7 +155,6 @@ class PyPDFOCR(object):
         self.pdf_filename = args.pdf_filename
         self.lang = args.lang
         self.watch_dir = args.watch_dir
-        self.enable_email = args.mail
         self.match_using_filename = args.match_using_filename
 
 
@@ -204,18 +179,6 @@ class PyPDFOCR(object):
             logging.debug("Read in configuration file")
             logging.debug(self.config)
 
-        if args.enable_evernote:
-            self.enable_evernote = True
-        else:
-            self.enable_evernote = False
-
-        if args.enable_filing or args.enable_evernote:
-            self.enable_filing = True
-            if not args.configfile:
-                p.error("Please specify a configuration file(CONFIGFILE) to enable filing")
-        else:
-            self.enable_filing = False
-
         self.watch = False
 
         if args.watch_dir:
@@ -238,79 +201,7 @@ class PyPDFOCR(object):
                 os.remove(f)
             except:
                 logging.debug("Error removing file %s .... continuing" % f)
-
-            
-
-    def _setup_filing(self):
-        """
-            Instance the proper PyFiler object (either
-            :class:`pypdfocr.pypdfocr_filer_dirs.PyFilerDirs` or
-            :class:`pypdfocr.pypdfocr_filer_evernote.PyFilerEvernote`)
-
-            TODO: Make this more generic to allow third-party plugin filing objects
-
-            :ivar filer: :class:`pypdfocr.pypdfocr_filer.PyFiler` PyFiler subclass object that is instantiated
-            :ivar pdf_filer: :class:`pypdfocr.pypdfocr_pdffiler.PyPdfFiler` object to help with PDF reading
-            :returns: Nothing
-
-        """
-        # Look at self.config and create a self.pdf_filer object
-
-        # --------------------------------------------------
-        # Some sanity checks
-        # --------------------------------------------------
-        assert(self.config and self.enable_filing)
-        for required in ['target_folder', 'default_folder']:
-            if not required in self.config:
-                error ("%s must be specified in config file" % required)
-            else:
-                # Make sure these required folders are in abspath format
-                self.config[required] = os.path.abspath(self.config[required])
-        if 'original_move_folder' in self.config:
-            # User wants to move the original after filing
-            orig = 'original_move_folder'
-            self.config[orig] = os.path.abspath(self.config[orig])
-            if not os.path.exists(self.config[orig]):
-                os.makedirs(self.config[orig])
-            original_move_folder = self.config[orig]
-        else:
-            original_move_folder = None
-        # --------------------------------------------------
-        # Start the filing object
-        # --------------------------------------------------
-        if self.enable_evernote:
-            self.filer = PyFilerEvernote(self.config['evernote_developer_token'])
-        else:
-            self.filer = PyFilerDirs()
-            
-        self.filer.target_folder = self.config['target_folder']
-        self.filer.default_folder = self.config['default_folder']
-        self.filer.original_move_folder = original_move_folder
-
-        self.pdf_filer = PyPdfFiler(self.filer)
-        if self.match_using_filename:
-            print("Matching using filename as a fallback to pdf contents")
-            self.pdf_filer.file_using_filename = True
-
-        # ------------------------------
-        # Add all the folder names with associated keywords
-        # to the filer object
-        # ------------------------------
-        keyword_count = 0
-        folder_count = 0
-        if 'folders' in self.config:
-            for folder, keywords in self.config['folders'].items():
-                folder_count +=1
-                keyword_count += len(keywords)
-                # Make sure keywords are lower-cased before adding
-                keywords = [str(x).lower() for x in keywords]
-                self.filer.add_folder_target(folder, keywords)
-
-        print ("Filing of PDFs is enabled")
-        print (" - %d target filing folders" % (folder_count))
-        print (" - %d keywords" % (keyword_count))
-
-    
+  
     def _setup_external_tools(self):
         """
             Instantiate the external tool wrappers with their config dicts
@@ -387,56 +278,6 @@ class PyPDFOCR(object):
         print ("Completed conversion successfully to %s" % ocr_pdf_filename)
         return ocr_pdf_filename
 
-    def file_converted_file(self, ocr_pdffilename, original_pdffilename):
-        """ move the converted filename to its destiantion directory.  Optionally also
-            moves the original PDF.
-
-            :param ocr_pdffilename: Converted PDF file
-            :type ocr_pdffilename: filename string
-            :param original_pdffilename: Original scanned PDF file
-            :type original_pdffilename: filename string
-            :returns: Target folder name
-            "rtype: string
-        """
-        filed_path = self.pdf_filer.move_to_matching_folder(ocr_pdffilename)  
-        print("Filed %s to %s as %s" % (ocr_pdffilename, os.path.dirname(filed_path), os.path.basename(filed_path)))
-
-        tgt_path = self.pdf_filer.file_original(original_pdffilename)
-        if tgt_path != original_pdffilename:
-            print("Filed original file %s to %s as %s" % (original_pdffilename, os.path.dirname(tgt_path), os.path.basename(tgt_path)))
-        return os.path.dirname(filed_path)
-
-  
-    def _send_email(self, infilename, outfilename, filing ):
-        """
-            Send email using smtp
-        """
-        print("Sending email status")
-        from_addr = self.config["mail_from_addr"]
-        to_addr_list = self.config["mail_to_list"]
-        smtpserver = self.config["mail_smtp_server"]
-        login = self.config["mail_smtp_login"]
-        password = self.config["mail_smtp_password"]
-
-        subject = "PyPDFOCR converted: %s" % (os.path.basename(outfilename))
-        header  = 'From: %s\n' % login
-        header += 'To: %s\n' % ','.join(to_addr_list)
-        header += 'Subject: %s\n\n' % subject
-        message = """
-        PyPDFOCR Conversion:
-        --------------------
-        Original file: %s
-        Converted file: %s
-        Filing: %s
-        """ % (infilename, outfilename, filing)
-        message = header + message
-      
-        server = smtplib.SMTP(smtpserver)
-        server.starttls()
-        server.login(login,password)
-        problems = server.sendmail(from_addr, to_addr_list, message)
-        server.quit()
-
     def go(self, argv):
         """ 
             The main entry point into PyPDFOCR
@@ -445,7 +286,6 @@ class PyPDFOCR(object):
             #. If filing is enabled, call :func:`_setup_filing`
             #. If watch is enabled, start the watcher
             #. :func:`run_conversion`
-            #. if filing is enabled, call :func:`file_converted_file`
         """
         # Read the command line options
         self.get_options(argv)
@@ -478,13 +318,7 @@ class PyPDFOCR(object):
             Helper function to run the conversion, then do the optional filing, and optional emailing.
         """
         ocr_pdffilename = self.run_conversion(pdf_filename)
-        if self.enable_filing:
-            filing = self.file_converted_file(ocr_pdffilename, pdf_filename)
-        else:
-            filing = "None"
-
-        if self.enable_email:
-            self._send_email(pdf_filename, ocr_pdffilename, filing)
+        filing = "None"
 
 def main(): # pragma: no cover 
     multiprocessing.freeze_support()
